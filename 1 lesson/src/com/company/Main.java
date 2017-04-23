cfa00fcfc82c262b81d6358c4fea6a822abf9183
0legKuznetsov <kuz.v.oleg@gmail.com>
2017-04-23 22:27:21 +0300

    1 lesson
    1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
    
    2. Создать переменные всех пройденных типов данных, и инициализировать
    их значения;
    
    3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий
    результат,где a, b, c, d – входные параметры этого метода;
    
    4. Написать метод, принимающий на вход два числа, и проверяющий что их
    сумма лежит в пределах от 10 до 20(включительно), если да – вернуть
    true, в противном случае – false;
    
    5. Написать метод, которому в качестве параметра передается целое число,
    метод должен напечатать в консоль положительное ли число передали, или
    отрицательное; Замечание: ноль считаем положительным числом.
    
    6. Написать метод, которому в качестве параметра передается целое число,
    метод должен вернуть true, если число отрицательное;
    
    7. Написать метод, которому в качестве параметра передается строка,
    обозначающая имя, метод должен вывести в консоль сообщение «Привет,
    указанное_имя!»;
    
    8. * Написать метод, который определяет является ли год високосным, и
    выводит сообщение в консоль. Каждый 4-й год является високосным, кроме
    каждого 100-го, при этом каждый 400-й – високосный.1. Создать пустой
    проект в IntelliJ IDEA и прописать метод main();
    
    2. Создать переменные всех пройденных типов данных, и инициализировать
    их значения;
    
    3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий
    результат,где a, b, c, d – входные параметры этого метода;
    
    4. Написать метод, принимающий на вход два числа, и проверяющий что их
    сумма лежит в пределах от 10 до 20(включительно), если да – вернуть
    true, в противном случае – false;
    
    5. Написать метод, которому в качестве параметра передается целое число,
    метод должен напечатать в консоль положительное ли число передали, или
    отрицательное; Замечание: ноль считаем положительным числом.
    
    6. Написать метод, которому в качестве параметра передается целое число,
    метод должен вернуть true, если число отрицательное;
    
    7. Написать метод, которому в качестве параметра передается строка,
    обозначающая имя, метод должен вывести в консоль сообщение «Привет,
    указанное_имя!»;
    
    8. * Написать метод, который определяет является ли год високосным, и
    выводит сообщение в консоль. Каждый 4-й год является високосным, кроме
    каждого 100-го, при этом каждый 400-й – високосный.

--- 1 lesson/src/com/company/Main.java
+++ 1 lesson/src/com/company/Main.java
@@ -23,6 +23,28 @@ public class Main {
         }
     }
 
+    public static boolean chk_digit(int z) {
+        if (z < 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void printTypeDigit(boolean v, int z){
+        if (v){
+            System.out.println("Число " + z + " отрицательное");
+        }
+        else {
+            System.out.println("Число " + z + " положительное");
+        }
+    }
+
+    public static void printMyName(String txtToPrint){
+        System.out.println("Привет, " + txtToPrint);
+    }
+
     public static boolean chk_leap_year(int y){
         if (y%4 == 0) {
             if (y%400 == 0) {
@@ -46,27 +68,45 @@ public class Main {
         // declare variables
         byte    a = 1;
         short   b = 32767;
-        int     c, y; ;
+        int     c, y, z; ;
         long    d = 1048576L;
         float   e = 12.3f;
         double  f = 15.72d;
-        boolean g = true;
+        boolean g, v;
         char    h = 'A';
+        String  txtToPrint;
+
 
         // constant
         final int x = 20;
 
         c = 1048576;
+        g = true;
         y = 2017;
+        z = -120;
+        txtToPrint = "Олег";
 
+        //Вывод результата арифметических действий
         System.out.println("calculate = " + calculate(a, b, c, d));
+
+        //Вывод результата проверка в каком диапазоне находится сумма чисел
         System.out.println("chk_sum = " + chk_sum(a, b));
 
+        //Проверка на весокосный год
         if (chk_leap_year(y)){
             System.out.println(y + " весокосный год" );
         }
         else {
             System.out.println(y + " обыкновенный год" );
         }
+
+        //Вывод имени в консоль
+        printMyName(txtToPrint);
+
+        //Проверка отрицательное или положительное число было передано
+        System.out.println("chk_digit = " + chk_digit(z));
+
+        //Вывод положительное или отрицательное число
+        printTypeDigit(chk_digit(z), z);
     }
 }
